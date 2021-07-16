4f114a56dc1fde9178c3951b2860ab263e34c826
Manuel Figueira <manuel.figueira.9@gmail.com>
2021-07-16 13:06:20 -0300

    added videos

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = 'AIzaSyAMb6-Qk2abtL3Tm-PUC54c6-LPFDAzsqQ';
+const API_KEY = 'API_KEY';
 
 export default axios.create({
   baseURL: 'https://www.googleapis.com/youtube/v3',
